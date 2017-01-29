63745f458238c8d3890139f2097374ca230b44ae
irdvo <dvoudheusden@gmail.com>
2017-01-29 10:40:24 +0100

    GeoLocationDialog cont.

--- GeoLocationDialog.cpp
+++ GeoLocationDialog.cpp
@@ -21,34 +21,39 @@ GeoLocationDialog::GeoLocationDialog(QWidget *parent) :
 
       _filenameEdit = new QLineEdit;
       _filenameEdit->setReadOnly(true);
-      grid->addWidget(_filenameEdit, 0, 1);
+      grid->addWidget(_filenameEdit, 0, 1, 1, 4);
 
       _browseButton = new QPushButton(tr("&Browse..."));
       connect(_browseButton, SIGNAL(clicked()), this, SLOT(browse()));
-      grid->addWidget(_browseButton, 0, 2);
+      grid->addWidget(_browseButton, 0, 5);
 
       grid->addWidget(new QLabel(tr("Tracks:")), 1, 0);
       _tracksEdit = new QLineEdit;
       _tracksEdit->setEnabled(false);
-      grid->addWidget(_tracksEdit, 1, 1, 1, 2);
+      _tracksEdit->setAlignment(Qt::AlignRight);
+      grid->addWidget(_tracksEdit, 1, 1);
 
-      grid->addWidget(new QLabel(tr("Segments:")), 2, 0);
+      grid->addWidget(new QLabel(tr("Segments:")), 1, 2);
       _segmentsEdit = new QLineEdit;
       _segmentsEdit->setEnabled(false);
-      grid->addWidget(_segmentsEdit, 2, 1, 1, 2);
+      _segmentsEdit->setAlignment(Qt::AlignRight);
+      grid->addWidget(_segmentsEdit, 1, 3);
 
-      grid->addWidget(new QLabel(tr("Points:")), 3, 0);
+      grid->addWidget(new QLabel(tr("Points:")), 1, 4);
       _pointsEdit = new QLineEdit;
       _pointsEdit->setEnabled(false);
-      grid->addWidget(_pointsEdit, 3, 1, 1, 2);
+      _pointsEdit->setAlignment(Qt::AlignRight);
+      grid->addWidget(_pointsEdit, 1, 5);
 
     vbox->addLayout(grid);
 
-    QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
-    vbox->addWidget(buttonBox);
+    _buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    _buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
 
-    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
-    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    vbox->addWidget(_buttonBox);
+
+    connect(_buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
+    connect(_buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
 }
 
 GeoLocationDialog::~GeoLocationDialog()
@@ -64,7 +69,129 @@ void GeoLocationDialog::browse()
   if (!filename.isNull())
   {
     _filenameEdit->setText(filename);
+
+    QFile xmlFile(filename);
+
+    if (xmlFile.open(QIODevice::ReadOnly))
+    {
+      int tracks   = 0;
+      int segments = 0;
+
+      parseXML(xmlFile, tracks, segments, _points);
+
+      _tracksEdit  ->setText(tr("%1").arg(tracks));
+      _segmentsEdit->setText(tr("%1").arg(segments));
+      _pointsEdit  ->setText(tr("%1").arg(_points.size()));
+
+      _buttonBox->button(QDialogButtonBox::Ok)->setEnabled(_points.size() > 0);
+    }
   }
+}
+
+void GeoLocationDialog::parseXML(QFile &xmlFile, int &tracks, int &segments, QVector<TrkPt> &points)
+{
+  QXmlStreamReader xml;
+
+  xml.setDevice(&xmlFile);
+
+  tracks   = 0;
+  segments = 0;
+
+  points.clear();
+
+  xml.readNext();
+  while (!xml.atEnd())
+  {
+    if ((xml.isStartElement()) && (xml.name().compare("trk", Qt::CaseInsensitive) == 0))
+    {
+      parseTrk(xml, segments, points);
+      tracks++;
+    }
+
+    xml.readNext();
+  }
+}
+
+void GeoLocationDialog::parseTrk(QXmlStreamReader &xml, int &segments, QVector<TrkPt> &points)
+{
+  xml.readNext();
+  while ((!xml.isEndElement()) || (xml.name().compare("trk", Qt::CaseInsensitive) != 0))
+  {
+    if ((xml.isStartElement()) && (xml.name().compare("trkseg", Qt::CaseInsensitive) == 0))
+    {
+      parseTrkSeg(xml, points);
+      segments++;
+    }
+
+    xml.readNext();
+  }
+}
+
+void GeoLocationDialog::parseTrkSeg(QXmlStreamReader &xml, QVector<TrkPt> &points)
+{
+  xml.readNext();
+  while ((!xml.isEndElement()) || (xml.name().compare("trkseg", Qt::CaseInsensitive) != 0))
+  {
+    if ((xml.isStartElement()) && (xml.name().compare("trkpt", Qt::CaseInsensitive) == 0))
+    {
+      TrkPt trkPt;
+      bool  valid = true;
+
+      if (xml.attributes().hasAttribute("lat"))
+      {
+        trkPt.latitude = xml.attributes().value("lat").toDouble();
+      }
+      else
+      {
+        valid = false;
+      }
+
+      if (xml.attributes().hasAttribute("lon"))
+      {
+        trkPt.longitude = xml.attributes().value("lon").toDouble();
+      }
+      else
+      {
+        valid = false;
+      }
+
+      if (!parseTime(xml, trkPt.secsToEpoch))
+      {
+          valid =false;
+      }
+
+      if (valid)
+      {
+        points.append(trkPt);
+      }
+    }
+
+    xml.readNext();
+  }
+}
+
+bool GeoLocationDialog::parseTime(QXmlStreamReader &xml, qint64 &secsToEpoch)
+{
+  bool valid = false;
+
+  while ((!xml.isEndElement()) || (xml.name().compare("trkpt", Qt::CaseSensitive) != 0))
+  {
+    if ((xml.isStartElement()) && (xml.name().compare("time", Qt::CaseSensitive) == 0))
+    {
+      QDateTime dateTime = QDateTime::fromString(xml.text().toString(), Qt::ISODate);
+
+#if QT_VERSION >= 0x050800
+      secsToEpoch = dateTime.toSecsSinceEpoch();
+#else
+      secsToEpoch = dateTime.toMSecsSinceEpoch() / 1000;
+#endif
+      valid = true;
+    }
+
+    xml.readNext();
+  }
+
+  return valid;
 }
 
 // ============================================================================
--- GeoLocationDialog.h
+++ GeoLocationDialog.h
@@ -5,6 +5,9 @@
 
 class QLineEdit;
 class QPushButton;
+class QFile;
+class QXmlStreamReader;
+class QDialogButtonBox;
 
 class GeoLocationDialog : public QDialog
 {
@@ -17,12 +20,31 @@ public:
 private slots:
   void browse();
 
+public:
+  struct TrkPt
+  {
+    double latitude;
+    double longitude;
+    qint64 secsToEpoch;
+  };
+
+  QVector<TrkPt> &trkPts() { return _points; }
+
 private:
-  QLineEdit    *_filenameEdit;
-  QPushButton  *_browseButton;
-  QLineEdit    *_tracksEdit;
-  QLineEdit    *_segmentsEdit;
-  QLineEdit    *_pointsEdit;
+  void parseXML(QFile &xmlFile, int &tracks, int &segments, QVector<TrkPt> &_points);
+  void parseTrk(QXmlStreamReader &xml, int &segments, QVector<TrkPt> &_points);
+  void parseTrkSeg(QXmlStreamReader &xml, QVector<TrkPt> &_points);
+  bool parseTime(QXmlStreamReader &xml, qint64 &secsToEpoch);
+
+private:
+  QVector<TrkPt>    _points;
+
+  QLineEdit        *_filenameEdit;
+  QPushButton      *_browseButton;
+  QLineEdit        *_tracksEdit;
+  QLineEdit        *_segmentsEdit;
+  QLineEdit        *_pointsEdit;
+  QDialogButtonBox *_buttonBox;
 };
 
 #endif
